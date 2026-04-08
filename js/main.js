dd333ac28aa80e073f31c8434cc6a759359c80bb
Jhonatan <fc75475927@gmail.com>
2026-04-08 05:56:55 -0400

    feat: implementacino de funcionalidad de tiempo transcurrido

--- js/main.js
+++ js/main.js
@@ -3,22 +3,38 @@ import { Player } from "./nave.js";
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
+const scoreElement = document.getElementById("scoreValue");
+const timeElement = document.getElementById("timeValue");
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const player = new Player(canvas.width / 2, canvas.height / 2);
 
+let score = 0;
+let startTime = Date.now();
+
 function gameLoop() {
-  update();
-  draw();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    update();
+    draw();
+
+    requestAnimationFrame(gameLoop);
 }
 
 function update() {
-  player.update();
+    player.update();
+    
+    const timeElapsed = Math.floor((Date.now() - startTime) / 1000);
+    timeElement.innerText = timeElapsed;
+
+    scoreElement.innerText = score;
 }
 
 function draw() {
-  player.draw(ctx);
+    player.draw(ctx);
 }
+
 
 gameLoop();
